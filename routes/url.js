2868952bd2ab05c7003bb82fc97213b8c5adb796
Laraib khan <114330724+Laraibkhan13@users.noreply.github.com>
2025-06-22 13:29:19 +0530

    routes updated

--- routes/url.js
+++ routes/url.js
@@ -1,11 +1,14 @@
 const express= require('express');
 const { handleGeneratedShortURL } = require('../controllers/url');
+const { handleGetShortURL} = require('../controllers/url');
 const { model } = require('mongoose');
 const router = express.Router();
 
 router.post('/',handleGeneratedShortURL);
+router.get('/:shortID',handleGetShortURL);
 
-model.exports = router;
+
+module.exports = router;
 // This code sets up an Express router for handling URL shortening requests.
 // It imports the necessary modules and defines a POST route that calls the `handleGeneratedShortURL` function from the controller.
 // The router is then exported for use in the main application file.
